d5de2980b02712fa3c35fd6626ef7324b8d535a7
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-04-04 05:36:52 +0100

    lifo cache

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""lifo cache"""
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """Defines a fifo caching system with a limit"""
+
+    def __init__(self):
+        """initialization"""
+        super().__init__()
+
+    def put(self, key, item):
+        """ Add an item in the cache
+        """
+        if key is not None and item is not None:
+            max = self.MAX_ITEMS
+            if len(self.cache_data) >= max:
+                first_key = next(iter(self.cache_data))
+                print("DISCARD: {}".format(first_key))
+                del self.cache_data[first_key]
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """ Get an item by key
+        """
+        if key is not None:
+            return self.cache_data.get(key)
+        return None

c5392d39d73db974dc74b0b0da6c7837877ba632
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-04-10 03:40:46 +0100

    force locale

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+"""app
+author
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+
+class Config:
+    """To configure the lang"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Gets the locale language"""
+    if 'locale' in request.args and request.args['locale'] in app.config['LANGUAGES']:
+        return request.args['locale']
+    else:
+        return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route('/')
+def get_index() -> str:
+    """The home/index page.
+    """
+    home_title = _('home_title')
+    home_header = _('home_header')
+    return render_template(
+            '4-index.html', home_title=home_title, home_header=home_header)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)

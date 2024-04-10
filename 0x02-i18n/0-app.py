ffba9a586d5b8ebe5ebaaeb3a4f230a338813f8b
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-04-10 01:32:25 +0100

    fixing2

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -4,12 +4,15 @@ from flask import Flask, render_template
 
 
 app = Flask(__name__)
-@app.route('/', strict_slashes=False)
-def home() -> str:
-    """to route to home page"""
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def get_index() -> str:
+    """The home/index page.
+    """
     return render_template('0-index.html')
 
 
-if __name__ == "__main__":
-    """ Main Function """
-    app.run()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)

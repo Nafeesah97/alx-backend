9d0eee3006f187229921b75cd5db33bac1ee300e
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-05-08 23:09:29 +0100

    job processor

--- 0x03-queuing_system_in_js/7-job_processor.js
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -5,67 +5,69 @@ const queue = kue.createQueue({ concurrency });
 const blacklisted = [4153518780, 4153518781];
 
 const jobs = [
-    {
-      phoneNumber: '4153518743',
-      message: 'This is the code 4321 to verify your account'
-    },
-    {
-      phoneNumber: '4153538781',
-      message: 'This is the code 4562 to verify your account'
-    },
-    {
-      phoneNumber: '4153118782',
-      message: 'This is the code 4321 to verify your account'
-    },
-    {
-      phoneNumber: '4153718781',
-      message: 'This is the code 4562 to verify your account'
-    },
-    {
-      phoneNumber: '4159518782',
-      message: 'This is the code 4321 to verify your account'
-    },
-    {
-      phoneNumber: '4158718781',
-      message: 'This is the code 4562 to verify your account'
-    },
-    {
-      phoneNumber: '4153818782',
-      message: 'This is the code 4321 to verify your account'
-    },
-    {
-      phoneNumber: '4154318781',
-      message: 'This is the code 4562 to verify your account'
-    },
-    {
-      phoneNumber: '4151218782',
-      message: 'This is the code 4321 to verify your account'
-    }
-  ];
+  {
+    phoneNumber: '4153518743',
+    message: 'This is the code 4321 to verify your account'
+  },
+  {
+    phoneNumber: '4153538781',
+    message: 'This is the code 4562 to verify your account'
+  },
+  {
+    phoneNumber: '4153118782',
+    message: 'This is the code 4321 to verify your account'
+  },
+  {
+    phoneNumber: '4153718781',
+    message: 'This is the code 4562 to verify your account'
+  },
+  {
+    phoneNumber: '4159518782',
+    message: 'This is the code 4321 to verify your account'
+  },
+  {
+    phoneNumber: '4158718781',
+    message: 'This is the code 4562 to verify your account'
+  },
+  {
+    phoneNumber: '4153818782',
+    message: 'This is the code 4321 to verify your account'
+  },
+  {
+    phoneNumber: '4154318781',
+    message: 'This is the code 4562 to verify your account'
+  },
+  {
+    phoneNumber: '4151218782',
+    message: 'This is the code 4321 to verify your account'
+  }
+];
 
 const sendNotification = (phoneNumber, message, job, done) => {
-  const totalProgress = 100;
+  const totalProgress = 50;
   let progressValue = 0; 
     
   const progressUpdateInterval = setInterval(() => {
-    job.progress(progressValue, totalProgress);
-    
-    console.log(`Notification job ${job.id} ${progressValue}% complete`);
-    
-    if (progressValue >= totalProgress) {
+    progressValue += 50;
+    if (progressValue <= totalProgress) {
+      job.progress(progressValue, totalProgress);
+    }
+    if (blacklisted.includes(phoneNumber)) {
+      done(new Error(`Phone number ${phoneNumber} is blacklisted`));
       clearInterval(progressUpdateInterval);
-      done();
+      return;
+    }
+    if (progressValue = 50) {
+      console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
     }
   }, 1000);
-  if (blacklisted.includes(phoneNumber)) {
-    done(new Error(`Phone number ${phoneNumber} is blacklisted`));
-  } else {
-    progressValue = 50;
-    console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
-  }
 };
 
 const pushNotificationQueue = queue;
+
+pushNotificationQueue.process('push_notification_code_2', 2, function(job, done) {
+  sendNotification(job.data.phoneNumber, job.data.message, job, done);
+});
 
 jobs.forEach(jobData => {
   const job = pushNotificationQueue.create('push_notification_code_2', jobData).save(function(err) {
